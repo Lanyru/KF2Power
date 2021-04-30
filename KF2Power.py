fcf3971b28da85a5f95afac8cab95673f6062f9c
Lanyru <Lanyru@protonmail.com>
2021-04-30 08:26:38 +0300

    Create KF2Power.py

---
+++ KF2Power.py
@@ -0,0 +1,22 @@
+from pymem  import *
+from pymem.process import *
+
+print("---Sirumal's Killing Floor 2 Power---")
+
+try:
+    pm = pymem.Pymem("Killing Floor 2 (64-bit, DX11) v1112")
+except:
+    sys.exit("Target process was not found!")
+
+gameModule = module_from_name(pm.process_handle, "KFGame.exe").lpBaseOfDll
+
+def GetPtrAddr(base, offsets):
+    addr = pm.read_int(base)
+    for i in offsets:
+        if i != offsets[-1]:
+            addr = pm.read_int(addr + i)
+    return addr + offsets[-1]
+    
+while True:
+    bullets = input("How many bullets do you want?\n")
+    pm.write_int(GetPtrAddr(gameModule + 0x0220A068, [0x420,0x40,0xD00,0x40,0x49C,0x244,0x5FC]), bullets)
